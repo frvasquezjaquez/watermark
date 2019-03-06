feed9d1305e47a8e2f46227afb1f72e62a63418d
Francisco J. Vasquez <fvasquez@argentuminc.com>
2019-03-05 23:16:30 -0400

    Watermar add logging

--- watermark.py
+++ watermark.py
@@ -1,8 +1,9 @@
 import os
 import sys
+import logging
 from PIL import Image
 
-
+logging.basicConfig(filename='imageWaterMark.log',level=logging.INFO)
 if len(sys.argv) < 3:
     print('Usage: watermark.py \'image folder path\' \'logo path\' [topleft, topright, bottomleft, bottomright, center]')
     sys.exit()
@@ -18,15 +19,23 @@ else:
 logo = Image.open(lgo)
 logoWidth = logo.width
 logoHeight = logo.height
-
+count = 0
 
 for filename in os.listdir(path):
     if (filename.endswith('.jpg') or filename.endswith('.png')) and (filename != lgo):
-
-        image = Image.open(path + '/' + filename)
+        try:
+            image = Image.open(path + '/' + filename)
+        except:
+            logging.error('Error abriendo image '+ filename)
+            
         imageWidth = image.width
         imageHeight = image.height
-
+        try:
+            image = image.resize((1080,1080), Image.ANTIALIAS)
+        except:
+             logging.error('Error durante image resize'+ filename)
+        count+=1
+        print count
         try:
             if pos == 'topleft':
                 image.paste(logo, (0, 0), logo)
@@ -35,17 +44,20 @@ for filename in os.listdir(path):
             elif pos == 'bottomleft':
                 image.paste(logo, (0, imageHeight - logoHeight), logo)
             elif pos == 'bottomright':
-                image.paste(logo, (imageWidth - logoWidth, imageHeight - logoHeight), logo)
+                image.paste(logo, ((imageWidth - logoWidth)/2, (imageHeight - logoHeight)/2), logo)
             elif pos == 'center':
                 image.paste(logo, ((imageWidth - logoWidth)/2, (imageHeight - logoHeight)/2), logo)
+            elif pos == 'centralized':
+                image.paste(logo, (0, 0), logo)
+
             else:
                 print('Error: ' + pos + ' is not a valid position')
                 print('Usage: watermark.py \'image path\' \'logo path\' [topleft, topright, bottomleft, bottomright, center]')
 
-            image.save(path + '/' + filename)
+            image.save('image_new' + '/' + filename)
             print('Added watermark to ' + path + '/' + filename)
 
         except:
             image.paste(logo, ((imageWidth - logoWidth)/2, (imageHeight - logoHeight)/2), logo)
-            image.save(path + '/' + filename)
-            print('Added default watermark to ' + path + '/' + filename)
+            image.save('image_new' + '/' + filename)
+            print('Added default watermark to ' + 'image_new' + '/' + filename)
